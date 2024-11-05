942d604095b447acbc494539288fbb1eaa016025
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2024-11-05 09:12:45 +0200

    merge: combine about and header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from "./components/about/page";
 import Header from "./components/header-components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <About />
     </>
   );
 }
