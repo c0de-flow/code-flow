6e01a995cfa5144e3483abd4997b7bd565309438
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2024-11-07 14:47:20 +0200

    chore: some updates on meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import About from "./components/about/page";
 import Business from "./components/business/Business";
 import Header from "./components/header-components/Header";
@@ -6,6 +7,9 @@ import Team from "./components/our-team/Team";
 export default function Home() {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
       <Header />
       <About />
       <Team />
