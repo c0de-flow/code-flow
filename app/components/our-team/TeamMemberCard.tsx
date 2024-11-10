fe67921ce554f0416ff2e02dda332906a4375bf1
roshdyjr <ahmedrush96@gmail.com>
2024-11-10 06:50:55 +0200

    team overlay adjusted

--- app/components/our-team/TeamMemberCard.tsx
+++ app/components/our-team/TeamMemberCard.tsx
@@ -10,22 +10,24 @@ interface TeamMemberCardProps {
 
 function TeamMemberCard({ name, role, imgSrc }: TeamMemberCardProps) {
   return (
-    <div className="border flex flex-col justify-end items-center gap-2 border-borderColor w-[224px] h-[175px] relative hover:scale-105 hover:shadow-lg transition-transform duration-300 cursor-pointer">
+    <div className="border flex flex-col justify-end items-center gap-2 border-borderColor w-[224px] h-[175px] hover:scale-105 hover:shadow-lg transition-transform duration-300 cursor-pointer">
       {/* Image Container */}
-      <div className="absolute bottom-1/2 flex justify-center items-center bg-bodyBackground w-10/12">
-        {/* Image with overlay */}
+      <div className="relative flex justify-center items-center w-10/12">
         <Image
           src={imgSrc}
           alt={name}
           width={160}
           height={198}
-          className="relative py-2 z-10"
+          className="py-2 z-10"
         />
+        <div className="absolute bottom-0 w-full h-[55%] bg-bodyBackground"></div>
       </div>
 
       {/* Team Member Info */}
-      <p className="text-xl font-abhaya">{name}</p>
-      <p className="font-abhaya mb-2">{role}</p>
+      <div className="text-center z-10">
+        <p className="text-xl font-abhaya">{name}</p>
+        <p className="font-abhaya mb-2">{role}</p>
+      </div>
     </div>
   );
 }
