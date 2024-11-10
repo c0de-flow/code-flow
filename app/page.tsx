23b842d76a9db9a282b0c8aba040ef49b74807f1
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2024-11-10 09:43:32 +0200

    chore: calling the services component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import About from "./components/about/page";
 import Business from "./components/business/Business";
 import Header from "./components/header-components/Header";
 import Team from "./components/our-team/Team";
+import Services from "./components/services/page";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Team />
       <Business />
+      <Services />
     </>
   );
 }
