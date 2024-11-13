c364ed49c10ffbfa11d1f2784fac67622cde8ed4
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2024-11-13 12:36:21 +0200

    chore: some updates

--- app/constants/TeamMembers.tsx
+++ app/constants/TeamMembers.tsx
@@ -2,32 +2,33 @@ const teamMembers = [
   {
     name: "Ahmed Ali",
     role: "Backend Developer",
-    imgSrc: "/team/back-1.png",
+    imgSrc: "/team/back-2.png",
   },
   {
     name: "Mohamed Ali",
     role: "Frontend Developer",
-    imgSrc: "/team/front-1.png",
-  },
-  {
-    name: "Asmaa ahmed",
-    role: "Owner",
-    imgSrc: "/team/owner.png",
-  },
-  {
-    name: "Ahmed Ali",
-    role: "Backend Developer",
-    imgSrc: "/team/back-2.png",
-  },
-  {
-    name: "Ahmed Ali",
-    role: "Frontend Developer",
-    imgSrc: "/team/front-2.png",
-  },
-  {
-    name: "Ahmed Ali",
-    role: "Backend Developer",
     imgSrc: "/team/back-1.png",
+  },
+  {
+    name: "Muhamed Saleh",
+    role: "FullStack Lead Developer",
+    imgSrc: "/team/Mu-saleh.png",
+  },
+  {
+    name: "Ahmed Roshdy",
+    role: "Frontend Developer",
+    imgSrc: "/team/roshdy.png",
+  },
+  {
+    name: "Zahraa Abdalraheem",
+    role: "UI/UX Designer",
+    imgSrc: "/team/zahraa.png",
+  },
+
+  {
+    name: "Mohamed Sayed",
+    role: "Frontend Developer",
+    imgSrc: "/team/Mu-sayed.png",
   },
 ];
 
